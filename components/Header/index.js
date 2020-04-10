6c7a097c0076cfe910b459f1f5f12cdfac653c75
Leonard Ayala <leonardayala@Leonards-MBP.hsd1.ct.comcast.net>
2020-04-10 13:51:02 -0400

    completed no stretch

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,32 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    // elements created
+    const header= document.createElement('div');
+    const date= document.createElement('span');
+    const heading= document.createElement('h1');
+    const temp= document.createElement('span');
+
+    //structure created
+    header.appendChild(date);
+    header.appendChild(heading);
+    header.appendChild(temp);
+
+    //classes
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    //data
+    date.textContent= 'MARCH 28, 2019';
+    temp.textContent= '98°';
+    heading.textContent= 'Lambda Times';
+
+    return header;
+}//end func
+
+
+const headerCont= document.querySelector('.header-container');
+
+headerCont.appendChild(Header());
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,34 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+
+axios
+        .get('https://lambda-times-backend.herokuapp.com/topics')
+        .then(res => {
+                const topics= document.querySelector('.topics');
+                const topico= res.data.topics;   //destructure
+                // console.log(topico);
+
+                topico.forEach( elemento => {
+                        topics.appendChild(createTab(elemento) );
+                        // console.log(elemento);
+                })//end foreach
+                // console.log(res);
+        })
+        .catch(err => {
+                console.log('Error: ', err);
+        })
+
+function createTab(topic){
+        //created elements
+        const tab= document.createElement('div');
+
+        //classes addeddddd
+        tab.classList.add('tab');
+
+        //content added
+        tab.textContent= topic;
+
+        return tab;
+}//end function
